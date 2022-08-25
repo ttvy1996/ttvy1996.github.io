a180bc4ac56ef9368cf5a4b66bae1d87c83251b1
ttvy1996 <truongtrieuvycsdl01@gmail.com>
2022-08-25 23:14:22 +0700

    cap nhat html khoi san pham

--- 1.js
+++ 1.js
@@ -1,3 +1,11 @@
 $(function() {
-	
+	$(".xuong,.kn").click(function(event) {
+		$('body,html').animate({scrollTop:$('#khoiedu').offset().top},800);
+		return false;
+	});
+
+	$(".sp").click(function(event) {
+		$('body,html').animate({scrollTop:$('#khoisanpham').offset().top},800);
+		return false;
+	});
 });
